cc9c2c3b965225c870c4cabc740179d5b3da3470
DAI\Amantri <abhinav.mantri@williamoneilindia.com>
2018-02-10 00:51:20 +0530

    First Commit

--- djangoBooks/books/views.py
+++ djangoBooks/books/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+
+from .models import Comment
+from .forms import CommentForm
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    comments = Comment.objects.order_by('-date_added')
+    context = {'comments': comments}
+    return render(request, 'index.html', context)
 
 def sign(request):
-    return render(request,'sign.html')
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            new_comment = Comment(name=request.POST['name'],
+                                  comment=request.POST['comment'])
+            new_comment.save()
+            return redirect('index')
+    else:
+        form = CommentForm()
+
+    form = CommentForm()
+    context = {'form': form}
+    return render(request,'sign.html', context)
